3e2e92933b2e39ac6aca9c185d34abb71c904b99
Tomas <tsutavicius@gmail.com>
2020-01-08 22:06:02 +0200

    Added an attempt at check_gameboard_full()

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from colorama import Fore, init, Back
+
 init(autoreset=True)
 
 Game_board = []
+
+
 # u"\u25CF" is the code for a circle
 
 
@@ -24,5 +27,22 @@ def draw_game_board():
         print(Back.CYAN + ' ')
 
 
+# Checking if all spaces are filled with player's stuff, hopefully
+def check_gameboard_full():
+    global board_full
+    count = 0
+    board_full = False
+    for i in range(6):
+        for j in range(7):
+            if Game_board[i][j] == u"\u25CF":
+                count += 1
+    if count == 42:
+        board_full = True
+    print(board_full)  # just testing if it can count
+    print(count)
+    return board_full
+
+
 init_game_board()
 draw_game_board()
+check_gameboard_full()
