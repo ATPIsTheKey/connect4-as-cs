73bc20b166a08e3a59a67b789ce236d817e97b95
Ignas <iggy.butkus@gmail.com>
2020-01-08 21:36:29 +0200

    changed circles back to O's for easier representation and removed old design

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from colorama import Fore, init, Back
 init(autoreset=True)
 
-global Game_board
 Game_board = []
 
 
 # Game board array initialization
 def init_game_board():
+    global Game_board
     for i in range(6):
         Game_board.append([])
         for j in range(7):
             Game_board[i].append("O")
 
 
+# Game board drawing function
 def draw_game_board():
     for i in range(6):
         print(Back.CYAN + ' ', end='')
