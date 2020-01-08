b985da0275db40e775dbdae7f5caa3d5c44741be
Ignas <iggy.butkus@gmail.com>
2020-01-08 20:51:49 +0200

    Implemented colorama colors to board and tweaked the style

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from colorama import Fore, init
+from colorama import Fore, init, Back
 init(autoreset=True)
 
 global Game_board
@@ -10,19 +10,27 @@ def init_game_board():
     for i in range(6):
         Game_board.append([])
         for j in range(7):
-            Game_board[i].append(0)
+            Game_board[i].append("0")
 
 
-# Game board drawing function
+# Game board drawing function design 1
+# def draw_game_board():
+#     for i in range(6):
+#         print(Fore.GREEN + " || ", end='')
+#         for j in range(6):
+#             print(Back.CYAN + str(Game_board[i][j]), end=Fore.GREEN + '|')
+#         print(Back.CYAN + str(Game_board[i][6]), end=' ')
+#         print(Fore.GREEN + "||")
+#         print(Fore.GREEN + " ++---+--+--+--+--+--+---++")
+    # for i in range(8):
+    #     print(Fore.GREEN + " - -", end='')
+
+
 def draw_game_board():
     for i in range(6):
-        print(Fore.GREEN + " || ", end='')
         for j in range(6):
-            print(Game_board[i][j], end=Fore.GREEN + ' | ')
-        print(Game_board[i][6], end=' ')
-        print(Fore.GREEN + "||")
-    for i in range(8):
-        print(Fore.GREEN + " - -", end='')
+            print(Back.CYAN + Fore.BLACK + str(Game_board[i][j]), end=Back.CYAN + '  ')
+        print(Back.CYAN + Fore.BLACK + str(Game_board[i][6]))
 
 
 init_game_board()
