ac9cb899d4c7e391de25b584777fdb40fe18026c
Tomas <tsutavicius@gmail.com>
2020-01-10 21:11:45 +0200

    Added a possible variant of inputting position, inserting the value, and switching between players

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ def draw_game_board():
         '2': Fore.YELLOW + u"\u25CF"
     }
 
-    print(Back.BLUE+' ', end='')
+    print(Back.BLUE + ' ', end='')
     for v in range(6):
-        print(Back.BLUE + Fore.BLACK + str(board_markers[v]), end=Back.BLUE+'  ')
+        print(Back.BLUE + Fore.BLACK + str(board_markers[v]), end=Back.BLUE + '  ')
     print(Back.BLUE + Fore.BLACK + str(board_markers[6]), end='')
     print(Back.BLUE + ' ')
 
@@ -57,36 +57,34 @@ for k in range(4):
 for p in range(2, 6):
     Game_board[p][6] = 2
 
-
 # Input values into the game board and switches between players - variant 1
 
-'''
-current_player = 1
+
+# current_player = 1
 
 
-def input_value():
-    global current_player
-    x_position = input()
-    values = ['1', '2', '3', '4', '5', '6', '7']
-    count = 0
-    if x_position not in values:
-        print('ENTER A VALID NUMBER THROUGH 1-7 PLEASE')
-        input_value()
-    else:
-        x_position = int(x_position)
-    for i in range(6):
-        if Game_board[i][x_position - 1] != 0:
-            count += 1
-    if count == 6:
-        print('ERROR : COLUMN FULL : ENTER A VALID NUMBER')
-        input_value()
-    else:
-        if current_player % 2 == 1:
-            Game_board[5 - count][x_position - 1] = 1
-        else:
-            Game_board[5 - count][x_position - 1] = 2
-    current_player += 1
-'''
+# def input_value():
+#   global current_player
+#   x_position = input()
+#   values = ['1', '2', '3', '4', '5', '6', '7']
+#   count = 0
+#   if x_position not in values:
+#       print('ENTER A VALID NUMBER THROUGH 1-7 PLEASE')
+#       input_value()
+#   else:
+#       x_position = int(x_position)
+#   for i in range(6):
+#       if Game_board[i][x_position - 1] != 0:
+#           count += 1
+#   if count == 6:
+#       print('ERROR : COLUMN FULL : ENTER A VALID NUMBER')
+#       input_value()
+#   else:
+#       if current_player % 2 == 1:
+#           Game_board[5 - count][x_position - 1] = 1
+#       else:
+#           Game_board[5 - count][x_position - 1] = 2
+#   current_player += 1
 
 
 if __name__ == '__main__':
